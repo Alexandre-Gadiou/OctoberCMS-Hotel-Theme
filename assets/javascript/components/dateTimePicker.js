7be9e782e9e1bae7a8063fbcf32979bd32edf4b9
Alexandre Gadiou <alexandregadiou44@icloud.com>
2017-02-20 11:47:00 +0100

    add js for datepickers

--- assets/javascript/components/dateTimePicker.js
+++ assets/javascript/components/dateTimePicker.js
@@ -5,7 +5,8 @@ console.trace("app/components/dateTimePicker loaded");
 
 var init = function () {
     console.log("Launch module dateTimePicker ...");
-    $('.input-group.date').datetimepicker({
+    
+    var pickerConfig = {
         format: 'YYYY-MM-DD',
         locale: 'en',
         icons:{
@@ -19,7 +20,27 @@ var init = function () {
             clear: 'fa fa-trash',
             close: 'fa fa-remove'
         }
+    };
+    
+    var picker2Config = pickerConfig;
+    picker2Config['useCurrent'] = false; //Important! See issue #1075
+    
+    console.log(picker2Config);
+    
+    $('.simple-datepicker').datetimepicker(pickerConfig);
+    
+    var $picker1 = $(".linked-datepicker.picker1");
+    var $picker2 = $(".linked-datepicker.picker2");
+
+    $picker1.datetimepicker(pickerConfig);
+    $picker2.datetimepicker(picker2Config);
+    $picker1.on("dp.change", function (e) {
+        $picker2.data("DateTimePicker").minDate(e.date);
     });
+    $picker2.on("dp.change", function (e) {
+        $picker1.data("DateTimePicker").maxDate(e.date);
+    });
+    
 };
 
 module.exports = {
